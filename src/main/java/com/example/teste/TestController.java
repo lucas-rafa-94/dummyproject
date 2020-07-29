79022a3f405016ad4dfbe5d862b6866ddce0761d
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2020-07-29 15:52:20 -0300

    ajuste

--- src/main/java/com/example/teste/TestController.java
+++ src/main/java/com/example/teste/TestController.java
@@ -19,6 +19,8 @@ public class TestController {
         System.out.println(req.getRequestURI());
         System.out.println(req.getRemoteHost());
         System.out.println(req.getHeader("referer"));
+        res.setHeader("Location", req.getHeader("referer"));
+        res.setStatus(302);
 //        return bdrSoftCaller.statusCall(callId, hubId);
     }
 }
